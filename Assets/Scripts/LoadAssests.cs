c8caed05fb4eb49f82150ef9010fdd3247c8f16b
kevinhuynh1206maker <kevinhuynh1206@gmail.com>
2025-08-25 18:49:59 +1000

    70 percent complete: instantiate prefabs + cap framerate

--- Assets/Scripts/LoadAssests.cs
+++ Assets/Scripts/LoadAssests.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
+    void Start() 
+    {
+        Application.targetFrameRate = 60;
+        Instantiate(redObj,  new Vector3( 2.2f, 0f, 0f), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2.2f, 0f, 0f), Quaternion.identity);
+    }
 }
