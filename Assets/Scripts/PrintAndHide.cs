405f4a84cd49c4ba26f4ccad384454f88174fec3
kevinhuynh1206maker <kevinhuynh1206@gmail.com>
2025-08-25 19:31:29 +1000

    Fix prefab renderer ref and editor

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
+    public Renderer rend;
+    
     private int i;
 
     void Start()
